2ee295cdbe5a88ac1cb3eec093002a5f6fc9dedc
Jonas Ulrich <136760+undefined@users.noreply.github.com>
2022-02-12 22:42:49 +0000

    feat: add working button

--- button/src/Button.tsx
+++ button/src/Button.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { Button as KDSButton, ButtonProps } from '@kickstartds/base/lib/button';
 
-export const Button = ({ variant = 'solid', ...rest }: ButtonProps) => (
-  <KDSButton variant={variant} {...rest} />
-);
+/**
+  The Button component is used to trigger an action or event, such as submitting a form, opening a dialog, canceling an action, or performing a delete operation.
+*/
+export const Button = ({
+  label = 'Lorem',
+  variant = 'solid',
+  ...rest
+}: ButtonProps) => <KDSButton label={label} variant={variant} {...rest} />;
